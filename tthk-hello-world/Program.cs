5b963cb114133318c9351f585936283501128ec7
Nikolas Laus <bchik3@gmail.com>
2020-03-26 19:49:47 +0200

    Hello, %username%!

---
+++ tthk-hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tthk_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
