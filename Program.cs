3be2884b4cba876b4c7be76c046e069be000bd8c
Nikolas Laus <bchik3@gmail.com>
2020-03-26 20:26:08 +0200

    Person class

--- Program.cs
+++ Program.cs
@@ -6,9 +6,21 @@ namespace tthk_hello_world
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
             Console.Write("Enter your name: ");
-            String name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + "!");
+            person.name = Console.ReadLine();
+            Console.Write("How old are you? ");
+            person.age = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Hello, {0}! You are {1} y.o. ", person.name, person.age);
+            if (person.age >= 18)
+                Console.WriteLine("and your non-minor.");
+            else
+                Console.WriteLine("and your minor.");
         }
+    }
+    class Person
+    {
+        public string name;
+        public int age;
     }
 }
